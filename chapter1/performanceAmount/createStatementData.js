0d50de8928837d2375735d687442e9fa86791bab
KIM-DO-HEE <devkdhee@gmail.com>
2024-10-14 21:52:47 +0900

    refactor : volumneCredits을 계산기로 옮기기

--- chapter1/performanceAmount/createStatementData.js
+++ chapter1/performanceAmount/createStatementData.js
@@ -27,6 +27,15 @@ class PerformanceCalculator {
 
     return result
   }
+
+  get volumneCredits() {
+    let result = 0
+    // 포인트 적립
+    result += Math.max(this.performance.audience - 30, 0)
+    // 희극관객 5명마다 추가 포인트를 제공
+    if ('comedy' == this.play.type) result += Math.floor(this.performance.audience / 5)
+    return result
+  }
 }
 
 // 중간 데이터 생성을 전담
@@ -43,7 +52,7 @@ export default function createStatementData(invoice, plays) {
     const result = Object.assign({}, aPerformance)
     result.play = calculator.play
     result.amount = calculator.amount
-    result.volumneCredits = volumneCreditsFor(result)
+    result.volumneCredits = calculator.volumneCredits
     return result
   }
 
